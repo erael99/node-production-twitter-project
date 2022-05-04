6f51095e27723e456c285597c1fc3f3d198666f9
delano <delanonyotue@gmail.com>
2022-05-04 09:36:23 +0200

    add the key and the crl path for the certification

--- environment/production.js
+++ environment/production.js
@@ -2,6 +2,6 @@ const path = require('path');
 
 module.exports = {
     dbUrl: 'mongodb+srv://zen:az@cluster0.8vrua.mongodb.net/twitter',
-    cert: path.join(__dirname, ''),
-    key: path.join(__dirname, '')
+    cert: '/etc/letsencrypt/live/www.dnyotue-projects.site/fullchain.pem',
+    key: '/etc/letsencrypt/live/www.dnyotue-projects.site/privkey.pem'
 }
