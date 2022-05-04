653cd626d61650bea1e572b1697e9f0c021cbf87
delano <delanonyotue@gmail.com>
2022-05-04 16:22:54 +0200

    follows and unfollows

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,4 +1,5 @@
-const { createUser } = require('../queries/users.queries');
+const { createUser, findUserPerUsername, searchUsersPerUsername, addUserIdToCurrentUserFollowing, findUserPerId, removeUserIdToCurrentUserFollowing } = require('../queries/users.queries');
+const { getUserTweetsFormAuthorId } = require('../queries/tweets.queries');
 const path = require('path');
 const multer = require('multer');
 const upload = multer({ storage: multer.diskStorage({
@@ -9,6 +10,33 @@ const upload = multer({ storage: multer.diskStorage({
     cb(null, `${ Date.now() }-${ file.originalname }`);
   }
 }) })
+
+exports.userList = async (req, res, next) => {
+  try {
+    const search = req.query.search;
+    const users = await searchUsersPerUsername(search);
+    res.render('includes/search-menu', { users });
+  } catch(e) {
+    next(e);
+  }
+}
+
+exports.userProfile = async (req, res, next) => {
+  try {
+    const username = req.params.username;
+    const user = await findUserPerUsername(username);
+    const tweets = await getUserTweetsFormAuthorId(user._id);
+    res.render('tweets/tweet', { 
+      tweets, 
+      isAuthenticated: req.isAuthenticated(), 
+      currentUser: req.user, 
+      user, 
+      editable: false 
+    });
+  } catch(e) {
+    next(e);
+  }
+}
 
 exports.signupForm = (req, res, next) => {
   res.render('users/user-form', { errors: null, isAuthenticated: req.isAuthenticated(), currentUser: req.user });
@@ -37,3 +65,23 @@ exports.uploadImage = [
     }
   }
 ]
+
+exports.followUser = async (req, res, next) => {
+  try {
+    const userId = req.params.userId;
+    const [, user] = await Promise.all([ addUserIdToCurrentUserFollowing(req.user, userId), findUserPerId(userId)])
+    res.redirect(`/users/${ user.username }`);
+  } catch(e) {
+    next(e);
+  }
+}
+
+exports.unFollowUser = async (req, res, next) => {
+  try {
+    const userId = req.params.userId;
+    const [, user] = await Promise.all([ removeUserIdToCurrentUserFollowing(req.user, userId), findUserPerId(userId)])
+    res.redirect(`/users/${ user.username }`);
+  } catch(e) {
+    next(e);
+  }
+}
