bd82f13310c7753d6d7c7cfeeb2d0baa702239a3
delano <delanonyotue@gmail.com>
2022-05-04 10:11:30 +0200

    add retry true

--- environment/production.js
+++ environment/production.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    dbUrl: 'mongodb+srv://zen:az@cluster0.8vrua.mongodb.net/twitter',
-    cert: '/etc/letsencrypt/live/www.dnyotue-projects.site/fullchain.pem',
+    dbUrl: 'mongodb+srv://zen:az@cluster0.8vrua.mongodb.net/twitter?retryWrites=true',
+    cert: '/etc/letsencrypt/live/www.dnyotue-projects.site/fullchain.pem', 
     key: '/etc/letsencrypt/live/www.dnyotue-projects.site/privkey.pem'
 }
